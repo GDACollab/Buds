2015ed37f81ff94ddfb531d039b7dda789eec753
Ray Nelson <rayolinels@gmail.com>
2020-01-23 17:11:45 -0800

    Added a placeholder for a plant script tro go on the plants in the plant inventory

--- Buds/Assets/Scripts/Game Scripts/PlantInventory.cs
+++ Buds/Assets/Scripts/Game Scripts/PlantInventory.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/** <summary>Holds plants that are not growing right now in the cemetery</summary>
+/** <summary>A holder for plants that are not being grown in the cemetery.</summary>
  */
 
 public class PlantInventory : MonoBehaviour
 {
-    [Tooltip("The plants that are not growing right now in the cemetery")]
+    [Tooltip("The plants that are currently in the inventory")]
     public GameObject[] plants;
+
+    [Tooltip("The plant prefab that should be placed in the inventory")]
     public GameObject plantPrefab;
 
+    [Tooltip("The distance between each plant in the inventory")]
     public float displayDistance;
-
-    private float totalDistanceSoFar;
 
     // Start is called before the first frame update
     void Start()
     {
+        float totalDistanceSoFar = 0;
+
         for (int i = 0; i < plants.Length; i++) {
-            var newPlant = Instantiate(plantPrefab, transform);
-            newPlant.transform.localPosition = new Vector3(newPlant.transform.localPosition.x + totalDistanceSoFar, newPlant.transform.localPosition.y, newPlant.transform.localPosition.z);
+            plants[i] = Instantiate(plantPrefab, transform);
+            plants[i].transform.localPosition =
+                new Vector3(plants[i].transform.localPosition.x + totalDistanceSoFar,
+                    plants[i].transform.localPosition.y,
+                    plants[i].transform.localPosition.z);
             totalDistanceSoFar += displayDistance;
         }
-        transform.position = new Vector3(transform.position.x + (-1 * (totalDistanceSoFar - 1) / 2), transform.position.y, transform.position.z);
+        transform.position =
+            new Vector3(transform.position.x + (-1 * (totalDistanceSoFar - 1) / 2),
+                transform.position.y,
+                transform.position.z);
     }
 
     // Update is called once per frame
