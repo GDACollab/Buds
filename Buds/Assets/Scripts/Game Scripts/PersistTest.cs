1d39b57f9d48792fa6bbef1ce5a0d97e02812d6d
Benjamin Roisen <broisen@ucsc.edu>
2020-01-25 02:13:37 -0800

    Added basic implementation of persistent data
    Added basic version of persistent data. Persistent fields are stored in "Assets/Scripts/Game Scripts/PersistentData.cs", and can be accessed by referencing "PersistentData.instance.varNameHere"
    
    Also added two test scenes (TestPersistent1 and TestPersistent2) and PersistTest.cs to confirm functionality of persistent data, as well as serve as examples of how to use the singleton.

---
+++ Buds/Assets/Scripts/Game Scripts/PersistTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Primarily used to test the PersistentData singleton, but also serves as an example of how to use it.
+/// NOTE: these functions were written assuming that they would be mapped to UI buttons;
+/// see the TestPersistent1 and TestPersistent2 scenes for an example.
+/// </summary>
+public class PersistTest : MonoBehaviour
+{
+    //replace button text with the specified string
+    public void SetText(string text)
+    {
+    	Text txt = transform.Find("Text").GetComponent<Text>(); //get the button's text component
+    	txt.text = text; //replace its string with the one passed as an argument
+    }
+
+    //tests writing to persistent data
+    public void StoreVal(int value)
+    {
+    	PersistentData.instance.exampleVar = value; //general format for writing to the singleton
+    	SetText("Set exampleVar to " + value); //update button text to show the value we stored in the singleton
+    }
+
+    //tests reading from persistent data
+    public void readVal()
+    {
+    	SetText("Read exampleVar: " + PersistentData.instance.exampleVar); //general format for reading from the singleton
+    }
+
+    //changes between scenes to confirm persistence
+    public void swapScene(string sceneName)
+    {
+    	SceneManager.LoadScene(sceneName); //just call Unity's SceneManager. Probably jank, but okay for testing purposes.
+    }
+}
---
+++ Buds/Assets/Scripts/Game Scripts/PersistentData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Implements a singleton object that allows for the transfer of data between scenes.
+/// HOW TO USE:
+/// -To add a variable, simply declare it in the indicated area (line 19 as of this writing)
+///    -OPTIONAL: initialize it in the Initialize() function
+/// -To access a variable, reference "PersistentData.instance.yourVariableNameHere"
+/// See PersistTest.cs and the TestPersistent1 and TestPersistent2 scenes for an example of how to use the system
+/// </summary>
+public class PersistentData : MonoBehaviour
+{
+	//globally-accessible instance used to access the data.
+	//To access a variable, reference "PersistentData.instance.variableName"
+    public static PersistentData instance;
+
+    //put persistent variables here, ideally accompanied by a description of what they go to
+    public int exampleVar; //test variable. Used by PersistTest.cs to confirm basic functionality of the singleton 
+
+    private void Awake ()
+    {
+    	//implement the singleton pattern
+    	if (instance != null)
+    	{
+    		//if another singleton already exists, destroy the new one
+    		Destroy(this.gameObject);
+    	}
+    	else
+    	{
+    		//if there's not already another singleton, we become the singleton
+    		instance = this;
+    		DontDestroyOnLoad(this.gameObject); //make sure we persist through scene changes
+    		Initialize(); //initialize variables
+    	}
+    }
+
+    //Initialize variables here
+    private void Initialize()
+    {
+    	exampleVar = 0;
+    }
+}
