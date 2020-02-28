3af9dff71e5c5e3f75d57517c6eb1a19889ab95c
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-02-27 20:40:54 -0800

    Yarn Spinner predevelopment
    I hope you're ready for the jank, because we're gonna hit jank levels that shouldn't even be possible.

---
+++ Buds/Assets/Scripts/Dialog Scripts/DialogueUICustom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueUICustom : Yarn.Unity.DialogueUIBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override Dialogue.HandlerExecutionType RunLine(Yarn.Line line, IDictionary<string, string> strings, System.Action onComplete)
+    {
+        // Start displaying the line; it will call onComplete later
+        // which will tell the dialogue to continue
+        StartCoroutine(DoRunLine(line, strings, onComplete));
+        return Dialogue.HandlerExecutionType.PauseExecution;
+    }
+
+    private IEnumerator DoRunLine(Yarn.Line line, IDictionary<string, string> strings, System.Action onComplete)
+    {
+        Debug.Log(strings[line.ID]);    //this was a strange work-around Tino figured out. Maybe?
+        yield return new WaitForSeconds(1);
+        onLineComplete();
+    }
+
+    public override void RunOptions(Yarn.OptionSet optionsCollection, IDictionary<string, string> strings, System.Action<int> selectOption)
+    {
+        throw new System.NotImplementedException("NO OPTIONS SUPPORTED");
+    }
+
+    public override Dialogue.HandlerExecutionType RunCommand(Yarn.Command command, System.Action onComplete)
+    {
+        throw new System.NotImplementedException("NO COMMANDS SUPPORTED");
+    }
+
+}
