db337b39f367eb15bf4b4ad5408287e9cc003480
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-03-01 14:40:04 -0800

    Even more experimentation
    What we need to do is create a Yarn Command that allows us to use in-line buttons controlled by a horizontal layout group, maybe, to branch the dialogue. We might not need super huge modifications to the yarn system after all...

--- Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
+++ Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
@@ -11,16 +11,21 @@ public class DialogUICustom : Yarn.Unity.DialogueUIBehaviour
     public Button button;
     public Text buttonText;
     public bool goToNextLine;
-    public override Dialogue.HandlerExecutionType RunCommand(Command command, Action onCommandComplete)
+    public GameObject DialogContainer;
+    public DialgoueRunner dialogueRunner;
+
+    //called when the object is loaded
+    //This will be used to add universal custom commands, which are defined after all the public handler methods.
+    void Start()
     {
-        throw new NotImplementedException();
+        dialogueRunner.AddCommandHandler("create_text_button", CreateTextButton);
     }
 
     public override Dialogue.HandlerExecutionType RunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
     {
         // Start displaying the line; it will call onComplete later
         // which will tell the dialogue to continue
-        goToNextLine = false;
+        //goToNextLine = false;
         StartCoroutine(DoRunLine(line, strings, onLineComplete));
         return Dialogue.HandlerExecutionType.PauseExecution;
     }
@@ -29,32 +34,36 @@ public class DialogUICustom : Yarn.Unity.DialogueUIBehaviour
     {
         var lineText = strings[line.ID];
         Debug.Log(strings[line.ID]);
+        //add command functionality
         text.text = lineText;
         //yield return new WaitUntil(() => goToNextLine);
         yield return new WaitForSeconds(3);
         onLineComplete();
     }
 
-    public void GoToNext()
+    /// Run a command.
+    public override Dialogue.HandlerExecutionType RunCommand(Yarn.Command command, System.Action onComplete)
     {
-        goToNextLine = true;
+        throw new NotImplementedException("NO COMMAND SUPPORT");
+        
+        // Dispatch this command via the 'On Command' handler.
+        //onCommand?.Invoke(command.Text);
+
+        // Signal to the DialogueRunner that it should continue executing.
+        //return Dialogue.HandlerExecutionType.ContinueExecution;
     }
 
-
+    //RunOptions makes lists of buttons to choose options from. This game won't have that, so it's not a big deal.
     public override void RunOptions(OptionSet optionSet, IDictionary<string, string> strings, Action<int> onOptionSelected)
     {
         throw new NotImplementedException("NO OPTION SUPPORT");
+        //StartCoroutine(DoRunOptions(optionsCollection, strings, selectOption));
     }
 
-    // Start is called before the first frame update
-    void Start()
+    //Creates buttons in-line with the text that can be clicked on. We can make them look like regular text later
+    private void CreateTextButton(string[] parameters)
     {
-        
+        throw new NotImplementedException("IN TEXT OPTIONS NOT FINISHED");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
