4f6abf81294fa024b07c77f8c98a904d9d5d3a4e
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-03-02 11:37:07 -0800

    Additional Development
    lotta things to do

--- Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
+++ Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
@@ -5,65 +5,72 @@ using UnityEngine;
 using UnityEngine.UI;
 using Yarn;
 
-public class DialogUICustom : Yarn.Unity.DialogueUIBehaviour
-{
-    public Text text;
-    public Button button;
-    public Text buttonText;
-    public bool goToNextLine;
-    public GameObject DialogContainer;
-    public DialgoueRunner dialogueRunner;
-
-    //called when the object is loaded
-    //This will be used to add universal custom commands, which are defined after all the public handler methods.
-    void Start()
+namespace Yarn.Unity
+{ 
+    public class DialogUICustom : Yarn.Unity.DialogueUIBehaviour
     {
-        dialogueRunner.AddCommandHandler("create_text_button", CreateTextButton);
-    }
+        public Text text;
+        public Button button;
+        public Text buttonText;
+        public bool goToNextLine;
+        public GameObject DialogContainer;
+        //public DialgoueRunner dialogueRunner;
+        public GameObject DialogueRunner;
 
-    public override Dialogue.HandlerExecutionType RunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
-    {
-        // Start displaying the line; it will call onComplete later
-        // which will tell the dialogue to continue
-        //goToNextLine = false;
-        StartCoroutine(DoRunLine(line, strings, onLineComplete));
-        return Dialogue.HandlerExecutionType.PauseExecution;
-    }
+        Script dr;  //what's the thing that allows me to pull script components?
 
-    private IEnumerator DoRunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
-    {
-        var lineText = strings[line.ID];
-        Debug.Log(strings[line.ID]);
-        //add command functionality
-        text.text = lineText;
-        //yield return new WaitUntil(() => goToNextLine);
-        yield return new WaitForSeconds(3);
-        onLineComplete();
-    }
+        //called when the object is loaded
+        //This will be used to add universal custom commands, which are defined after all the public handler methods.
+        void Start()
+        {
+            dr = DialogueRunner.GetComponent<DialogueRunner>();
+            dr.AddCommandHandler("create_text_button", CreateTextButton);
+        }
 
-    /// Run a command.
-    public override Dialogue.HandlerExecutionType RunCommand(Yarn.Command command, System.Action onComplete)
-    {
-        throw new NotImplementedException("NO COMMAND SUPPORT");
+        public override Dialogue.HandlerExecutionType RunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
+        {
+            // Start displaying the line; it will call onComplete later
+            // which will tell the dialogue to continue
+            //goToNextLine = false;
+            StartCoroutine(DoRunLine(line, strings, onLineComplete));
+            return Dialogue.HandlerExecutionType.PauseExecution;
+        }
+
+        private IEnumerator DoRunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
+        {
+            var lineText = strings[line.ID];
+            Debug.Log(strings[line.ID]);
+            //add command functionality
+            text.text = lineText;
+            //yield return new WaitUntil(() => goToNextLine);
+            yield return new WaitForSeconds(3);
+            onLineComplete();
+        }
+
+        /// Run a command.
+        public override Dialogue.HandlerExecutionType RunCommand(Yarn.Command command, System.Action onComplete)
+        {
+            throw new NotImplementedException("NO COMMAND SUPPORT");
         
-        // Dispatch this command via the 'On Command' handler.
-        //onCommand?.Invoke(command.Text);
+            // Dispatch this command via the 'On Command' handler.
+            //onCommand?.Invoke(command.Text);
 
-        // Signal to the DialogueRunner that it should continue executing.
-        //return Dialogue.HandlerExecutionType.ContinueExecution;
-    }
+            // Signal to the DialogueRunner that it should continue executing.
+            //return Dialogue.HandlerExecutionType.ContinueExecution;
+        }
 
-    //RunOptions makes lists of buttons to choose options from. This game won't have that, so it's not a big deal.
-    public override void RunOptions(OptionSet optionSet, IDictionary<string, string> strings, Action<int> onOptionSelected)
-    {
-        throw new NotImplementedException("NO OPTION SUPPORT");
-        //StartCoroutine(DoRunOptions(optionsCollection, strings, selectOption));
-    }
+        //RunOptions makes lists of buttons to choose options from. This game won't have that, so it's not a big deal.
+        public override void RunOptions(OptionSet optionSet, IDictionary<string, string> strings, Action<int> onOptionSelected)
+        {
+            throw new NotImplementedException("NO OPTION SUPPORT");
+            //StartCoroutine(DoRunOptions(optionsCollection, strings, selectOption));
+        }
 
-    //Creates buttons in-line with the text that can be clicked on. We can make them look like regular text later
-    private void CreateTextButton(string[] parameters)
-    {
-        throw new NotImplementedException("IN TEXT OPTIONS NOT FINISHED");
-    }
+        //Creates buttons in-line with the text that can be clicked on. We can make them look like regular text later
+        private void CreateTextButton(string[] parameters)
+        {
+            throw new NotImplementedException("IN TEXT OPTIONS NOT FINISHED");
+        }
     
+    }
 }
