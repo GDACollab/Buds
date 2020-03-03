2ff74d33b828bbe99e12a1a3ac65117d3b5b876b
Ray Nelson <rayolinels@gmail.com>
2020-03-03 11:21:10 -0800

    Background music plays an intro and loop
    - new Audio Test scene: right now it's demoing the main menu music but also works with the shiori music
    - The two new audio clips are from the sound department and are for the main menu and visual novel encounters w/ the retail friend

---
+++ Buds/Assets/Scripts/Sound Scripts/BGMPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* <summary>
+ * Plays an intro and then a loop for background music.
+ * Intro and loop audio clips are set in the inspector.
+ * </summary>
+ */
+
+[RequireComponent(typeof(AudioSource))]
+public class BGMPlayer : MonoBehaviour
+{
+
+    public AudioClip intro;
+    public AudioClip loop;
+    private AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = intro;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    void FixedUpdate()
+    {
+        if(!audioSource.isPlaying) {
+            audioSource.clip = loop;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
