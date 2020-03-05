bf741f46956bc37bb21faa8d60148edee11b5ed2
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-03-05 11:05:42 -0800

    In-Line Text Options
    Ladies and Gentlemen, we got'em.
    
    But in all seriousness, the system still needs some editor and maybe scripting modification to make it look good. Also it doesn't jump dialogue all that well yet.

--- Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
+++ Buds/Assets/Scripts/Dialog Scripts/DialogUICustom.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using Yarn;
 
 namespace Yarn.Unity
-{ 
+{
     public class DialogUICustom : Yarn.Unity.DialogueUIBehaviour
     {
         public Text text;
@@ -15,7 +15,8 @@ namespace Yarn.Unity
         public bool goToNextLine;
         public GameObject DialogContainer;
 
-        int lineTextIndex;
+        public int lineTextIndex;
+        int activeButtons;
 
         void Start()
         {
@@ -34,12 +35,14 @@ namespace Yarn.Unity
         private IEnumerator DoRunLine(Line line, IDictionary<string, string> strings, Action onLineComplete)
         {
             var lineText = strings[line.ID];
+            TextUIReset();
+            //text.text = "";
             //Debug.Log(strings[line.ID]);
             //add command functionality
             //text.text = lineText
-            for(int i = 0; i < lineText.Length; ++i)
+            for (int i = 0; i < lineText.Length; ++i)
             {
-                if(lineText[i] == '{' && lineText[i+1] == '{')
+                if (lineText[i] == '{' && lineText[i + 1] == '{')
                 {
                     i = CreateTextButton(lineText, i);
                 }
@@ -48,9 +51,18 @@ namespace Yarn.Unity
                     text.text += lineText[i];
                 }
             }
+            //If the line has no buttons, continue in 3 seconds
+            //If it does, wait until one is clicked
             goToNextLine = false;
-            yield return new WaitUntil(() => goToNextLine);
-            //yield return new WaitForSeconds(3);
+            if (activeButtons == 0)
+            {
+                yield return new WaitForSeconds(3);
+                Debug.Log("Waiting...");
+            }
+            else
+            {
+                yield return new WaitUntil(() => goToNextLine);
+            }
             onLineComplete();
         }
 
@@ -64,7 +76,7 @@ namespace Yarn.Unity
         public override Dialogue.HandlerExecutionType RunCommand(Yarn.Command command, System.Action onComplete)
         {
             throw new NotImplementedException("NO COMMAND SUPPORT");
-        
+
             // Dispatch this command via the 'On Command' handler.
             //onCommand?.Invoke(command.Text);
 
@@ -85,17 +97,20 @@ namespace Yarn.Unity
         //Using | }} will go to the immediate next line of dialog, while using |YarnFileName.NextNodeName}} will jump to the NextNodeName node in the yarn file.
         private int CreateTextButton(string LineText, int index)
         {
-            throw new NotImplementedException("IN TEXT OPTIONS NOT FINISHED");
-            string buttonText;
-            string nextNode;
+            //local variables
             Button newButton;
             Text newText;
+            string buttonText = "";
+            string nextNode = "";
             bool breaker = false;
+            //This keeps both braces from being printed
             ++index;
+            ++index;
+
             //reads the text to be highlighted 
             while (!breaker)
             {
-                if(LineText[index] == '|')
+                if (LineText[index] == '|')
                 {
                     breaker = true;
                 }
@@ -106,23 +121,27 @@ namespace Yarn.Unity
                 ++index;
             }
             //reads the node to travel to when the button is clicked
-            while(LineText[index] != '}' && LineText[index + 1] != '}')
+            while (LineText[index] != '}' && LineText[index + 1] != '}')
             {
                 //please make sure to put in this second brace I haven't written an exception yet and it WILL explode if it isn't found
                 nextNode += LineText[index];
                 ++index;
             }
+
             //creates the actual new button and sets it text
             newButton = Instantiate(button, DialogContainer.transform);
             newButton.transform.GetChild(0).gameObject.GetComponent<Text>().text = buttonText;
+
             //gives the button proper functionality
-            if (nextNode == " ")
+            Debug.Log("NextNode is >" + nextNode + "<");
+            if (nextNode == " " || nextNode == "")
             {
                 //button advances dialogue if there's no node destination
                 newButton.onClick.AddListener(this.NextLine);
             }
             else
             {
+                throw new NotImplementedException("NODE JUMPING NOT SUPPORTED");
                 //DialogueRunner.NodeExists(node) returns true/false if the node is, in fact, a node.
                 //newButton.onClick.AddListener();//method that causes node jumps
                 //the yarn way is really complicated and I don't understand it
@@ -131,10 +150,40 @@ namespace Yarn.Unity
                 //But the script it looks for is DialogueRunner...?
             }
 
-            //creates a new text object for the rest of the line to go on.
+            //creates a new, blank, text object for the rest of the line to go on.
             newText = Instantiate(text, DialogContainer.transform);
             text = newText;
-            return index;
+            text.text = "";
+
+            ++activeButtons;
+            text.gameObject.SetActive(true);
+            newButton.gameObject.SetActive(true);
+            //we return index + 2 so the trailing braces aren't printed
+            return index + 2;
+        }
+
+        //Resets the objects in the UI so that new ones can be used for new lines
+        private void TextUIReset()
+        {
+            Text newText;
+
+            //deactivate all of the children of DialogContainer (i.e. all of the text UI elements)
+            //in a perfect world we'd destroy them, but then we don't have text/buttons to instantiate from
+            //if this causes legit performance issues I'll come back and fix it later
+            for (int i = 0; i < DialogContainer.transform.childCount; ++i)
+            {
+                //sets child i of DialogContainer to be inactive
+                DialogContainer.transform.GetChild(i).gameObject.SetActive(false);
+            }
+
+            //creates an new text to write things on
+            newText = Instantiate(text, DialogContainer.transform);
+            text = newText;
+
+            //set it to be active
+            //text objects don't inherit the SetActive method from game objects...?
+            text.gameObject.SetActive(true);
+            text.text = "";
         }
     }
 }
