7f2ac9b2c420f07f6464c41cfec03ef9c02963d8
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-05-15 15:33:11 -0700

    Basic Tutorial Functionality
    Not currently usable, due to UI elements being misspaced and not sized correctly at runtime, but the underlying scripts are there.
    
    Needs some improvment to not be awful to change (all of the system's objects are non-prefab objects that just hang in canvas space).

---
+++ Buds/Assets/Scripts/Level Scripts/Tutorial.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    GameObject[] tutorialObjects;
+    bool nextSection = false;
+
+    /*
+     *      This script runs a very basic tutorial. It looks for
+     *  all children of the container object and, starting with 
+     *  the first one, activates it until a child button is
+     *  clicked. It then does the next one, until it runs out.
+     *  Once it runs out, the tutorial is done.
+     */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!(bool)PersistentData.instance.ReadData("TutorialDone"))
+        {
+            PersistentData.instance.StoreData("TutorialDone", true);
+            tutorialObjects = GetChildrenAsObjects(this.gameObject);
+            StartCoroutine(TutorialRunner());
+        }
+    }
+
+    IEnumerator TutorialRunner()
+    {
+        for(int i = 0; i < tutorialObjects.Length; ++i)
+        {
+            tutorialObjects[i].SetActive(true);
+            yield return new WaitUntil( () => nextSection);
+            tutorialObjects[i].SetActive(false);
+            nextSection = false;
+        }
+
+        Debug.Log("Tutorial done!");
+    }
+
+    //smol helper to grab all of the tutorial's objects
+    //as an array, for easy batch activation/deactivation
+    GameObject[] GetChildrenAsObjects(GameObject parent)
+    {
+        GameObject[] childs = new GameObject[parent.transform.childCount];
+
+        for(int i = 0; i < parent.transform.childCount; ++i)
+        {
+            childs[i] = parent.transform.GetChild(i).gameObject;
+        }
+
+        return childs;
+    }
+
+    //When clicked, buttons activate this method,
+    //which continues the tutorial. I could use
+    //Unity Events, but uhhhh I don't know
+    //how to use those. I should learn.
+    public void ContiuneTutorial()
+    {
+        nextSection = true;
+    }
+}
