6f5ea465291395c3441ec19d37f10059aab8cb1e
Ray Nelson <rayolinels@gmail.com>
2020-05-15 20:54:06 -0700

    phone fade to white between scenes

--- Buds/Assets/Scripts/Level Scripts/OrderedSceneNavigator.cs
+++ Buds/Assets/Scripts/Level Scripts/OrderedSceneNavigator.cs
@@ -15,13 +15,23 @@ public class OrderedSceneNavigator : MonoBehaviour
 
     public Button confirmButton;
 
+    public Image fadeOutUIImage;
+    public enum FadeDirection { In, Out }
+
+
     private SortedList<float, int> sceneOrder;
-
     private int numCompleted;
-
     private bool menuEnabled;
-
     private System.DateTime date;
+    private readonly float fadeSpeed = 0.8f;
+
+    
+    private float fadeStartValue;
+    private float fadeEndValue;
+    private bool fadeCompleted;
+    private bool loading;
+    private bool fadeStarted;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake() {
         Comparer<float> descendingComparer = Comparer<float>.Create((x, y) => y.CompareTo(x));
@@ -31,6 +41,9 @@ public class OrderedSceneNavigator : MonoBehaviour
         FixedUpdate();
 
         RetrieveSchedule();
+
+        fadeOutUIImage.enabled = true;
+        StartCoroutine(Fade(FadeDirection.Out));
     }
 
     void FixedUpdate()
@@ -67,10 +80,7 @@ public class OrderedSceneNavigator : MonoBehaviour
     }
 
     public void LoadScene() {
-        SetSchedule();
-        int sceneIndex = sceneOrder.Values[numCompleted];
-        Debug.Log("Loading scene at index " + sceneIndex);
-        SceneManager.LoadSceneAsync(sceneIndex);
+        FadeLoadScene();
     }
 
     public void Reset() {
@@ -151,6 +161,7 @@ public class OrderedSceneNavigator : MonoBehaviour
                         confirmButton.transform.parent.GetChild(2).GetComponent<Text>().text = "14:00";
 
                         break;
+                        // case 3 is for if there is a "Start of Day" scene like at MC's bedroom or something
                     //case 3:
                     //    dnd.enabled = false;
                     //    scheduleItems[newOrder].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
@@ -164,4 +175,122 @@ public class OrderedSceneNavigator : MonoBehaviour
         }
     }
 
+
+
+
+    // Fade in a screen transition image, then load next scene
+    public void FadeLoadScene() {
+
+        // Start coroutine Fade, which in turn starts FinishLoadScene
+        loading = true;
+        fadeCompleted = false;
+        if (!fadeStarted)
+            StartCoroutine(Fade(FadeDirection.In));
+    }
+
+    // Fade out a SpriteRenderer as it disappears
+    public void FadeAway(SpriteRenderer sr) {
+        spriteRenderer = sr;
+        StartCoroutine(Fade(FadeDirection.Out, sr));
+    }
+
+    // Fade in a SpriteRenderer as it appears
+    public void FadeAppear(SpriteRenderer sr) {
+        spriteRenderer = sr;
+        sr.enabled = true;
+        print(sr);
+        StartCoroutine(Fade(FadeDirection.In, sr));
+    }
+
+    // Coroutine to fade an Image or SpriteRenderer
+    private IEnumerator Fade(FadeDirection direction, SpriteRenderer sr = null) {
+        // Set start and end values if just beginning to fade
+        if (!fadeStarted) {
+            if (direction == FadeDirection.Out)
+                fadeStartValue = 1;
+            else
+                fadeStartValue = 0;
+            fadeEndValue = 1 - fadeStartValue;
+        }
+        fadeStarted = true;
+
+        // Continue to fade in or out until done
+        if (direction == FadeDirection.Out) {
+            while (fadeStartValue >= fadeEndValue) {
+                if (sr == null)
+                    SetTransparencyImage(FadeDirection.Out);
+                else
+                    SetTransparencySR(FadeDirection.Out);
+                yield return null;
+            }
+
+            // Disable Image/SR once it has disappeared 
+            if (sr == null)
+                fadeOutUIImage.enabled = false;
+            else
+                sr.enabled = false;
+        }
+        else {
+            // Enable Image/SR before it appears
+            if (sr == null)
+                fadeOutUIImage.enabled = true;
+            else
+                sr.enabled = true;
+            while (fadeStartValue <= fadeEndValue) {
+                if (sr == null)
+                    SetTransparencyImage(FadeDirection.In);
+                else
+                    SetTransparencySR(FadeDirection.In);
+                yield return null;
+            }
+        }
+
+        // Load next scene once fade is complete if in a load transition
+        if (sr == null && loading && !fadeCompleted) {
+            SetSchedule();
+            int sceneIndex = sceneOrder.Values[numCompleted];
+            Debug.Log("Loading scene at index " + sceneIndex);
+            StartCoroutine(FinishLoadScene(sceneIndex));
+        }
+            
+        fadeCompleted = true;
+        fadeStarted = false;
+    }
+
+    // Coroutine to load a scene
+    IEnumerator FinishLoadScene(int sceneIndex) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncLoad.isDone)
+            yield return null;
+    }
+
+    // Helper function for setting transparency on an Image (UI element)
+    private void SetTransparencyImage(FadeDirection fadeDirection) {
+        fadeOutUIImage.color = new Color(
+            fadeOutUIImage.color.r,
+            fadeOutUIImage.color.g,
+            fadeOutUIImage.color.b,
+            fadeStartValue
+            );
+        if (fadeDirection == FadeDirection.Out)
+            fadeStartValue -= Time.deltaTime / fadeSpeed;
+        else
+            fadeStartValue += Time.deltaTime / fadeSpeed;
+    }
+
+    // Helper function for setting transparency on a SpriteRenderer
+    private void SetTransparencySR(FadeDirection fadeDirection) {
+        spriteRenderer.color = new Color(
+            spriteRenderer.color.r,
+            spriteRenderer.color.g,
+            spriteRenderer.color.b,
+            fadeStartValue
+            );
+        if (fadeDirection == FadeDirection.Out)
+            fadeStartValue -= Time.deltaTime / fadeSpeed;
+        else
+            fadeStartValue += Time.deltaTime / fadeSpeed;
+    }
 }
+
