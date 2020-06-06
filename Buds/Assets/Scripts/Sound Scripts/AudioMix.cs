6202840c36fc64ac6377cf842ac7ca64a6ec533a
Ray Nelson <rayolinels@gmail.com>
2020-06-05 18:46:38 -0700

    new graveyard music

---
+++ Buds/Assets/Scripts/Sound Scripts/AudioMix.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMix : MonoBehaviour {
+
+    public AudioMixer mixer;
+    public string group;
+
+    public void SetSound(float soundLevel) {
+        mixer.SetFloat(group, Mathf.Log(soundLevel) * 20);
+    }
+}
