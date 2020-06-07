29c4b8b9341bc36b692047f4f100071d66e7528e
Ray Nelson <rayolinels@gmail.com>
2020-06-07 15:04:36 -0700

    Audio sliders now sync properly across scenes

--- Buds/Assets/Scripts/Sound Scripts/AudioMix.cs
+++ Buds/Assets/Scripts/Sound Scripts/AudioMix.cs
@@ -7,6 +7,11 @@ public class AudioMix : MonoBehaviour {
     public string group;
 
     public void SetSound(float soundLevel) {
-        mixer.SetFloat(group, Mathf.Log(soundLevel) * 20);
+        mixer.SetFloat(group, Mathf.Log(soundLevel + 0.01f) * 20f);
+    }
+
+    void Start() {
+        mixer.GetFloat(group, out float val);
+        GetComponent<UnityEngine.UI.Slider>().value = Mathf.Exp(val / 20f + 0.01f);
     }
 }
